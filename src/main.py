b62bbe0ebc8e205fb76f40ab0bf93db79670a1d1
SzymonRudnik <115082111+SzymonRudnik@users.noreply.github.com>
2022-10-11 22:26:22 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
+#inlcude <iostream>
 
+using namespace std;
+
+int a,b;
+int main()
+{
+  cout << "Podaj pierwsza liczb: ";
+  cin >> a;
+  cout << "Podaj druga liczbe: ";
+  cin >> b;
+  cout << "Suma tych liczb to: " << a+b << endl;
+  
+  return 0;
+}
